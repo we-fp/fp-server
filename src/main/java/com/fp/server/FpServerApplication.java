9a054529b685a1de733779de1f21caabd87a4639
cangming.zheng <cangming.zheng@rootcloud.com>
2020-05-29 17:29:23 +0800

    test v2

---
+++ src/main/java/com/fp/server/FpServerApplication.java
@@ -0,0 +1,13 @@
+package com.fp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fp/server/FpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
